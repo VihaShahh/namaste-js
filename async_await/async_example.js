e49d2286eb43c4779eb1fab498a74fad39669512
VihaShahh <vihashah1103@gmail.com>
2025-09-22 23:23:48 +0530

    async 5

--- async_await/async_example.js
+++ async_await/async_example.js
@@ -27,3 +27,87 @@ handlePromise().catch(err => console.log(err)); // this will work as handlePromi
 // You call .json() → reads + parses response body → resolves with actual JSON.
 
 // You log the JSON object.
+
+async function greet(){
+    return "hello"
+}
+greet().then(res => console.log(res))
+//is equivalent to Promise.resolve("hello")//so actually // -> Promise { "hello" }
+// When the promise resolves, it carries the value "hello".
+
+// .then(res => …) gives you that resolved value (res = "hello").
+
+// So console.log(res) prints:
+
+// hello
+//======================
+
+async function herry(){
+    let delhiWeather = new Promise((resolve) =>
+    {
+        setTimeout(() =>{
+            resolve("27 degree")
+        }, 2000)
+    })
+
+    let bangalorWeather = new Promise((resolve) =>{
+        setTimeout(() =>{
+            resolve("21 degree")
+        }, 7000)
+    })
+
+    let delhiW = await delhiWeather;
+    let banw = await bangalorWeather;
+    return [delhiW, banw]
+}
+
+herry().then(val => console.log(val))
+//=====================================
+
+// 5. Sequential vs Parallel Execution
+// Sequential (one by one):
+async function sequentially(){
+    const a = await fetch("")
+    const b = await fetch("")
+}
+sequentially()
+
+//Parallel (faster with Promise.all):
+
+async function parellel() {
+    const [a,b] = await Promise.all([fetch(""), fetch("")])
+    console.log(a,b)
+}
+parellel()
+
+//=============================
+
+//loops with async await
+
+async function processNumber(no){
+    for(let i of no){
+        await new Promise(resolve =>setTimeout(resolve, 1000))
+    }
+}
+processNumber([1,2,3,46])
+//resolve => setTimeout(resolve, 1000)
+
+// Here you are using arrow function syntax.
+
+// resolve is a function provided by JavaScript to fulfill the Promise.
+
+// setTimeout(resolve, 1000) means:
+// “After 1000 ms (1 second), call resolve().”
+
+// here processNumber(1234);
+// then no would be just a number, not iterable, and your code
+
+// js
+// Copy code
+// for (let i of no)
+// would throw an error → TypeError: no is not iterable.
+
+// Confirming with code
+// js
+// Copy code
+// console.log(Array.isArray([1,2,3,46])); // true
